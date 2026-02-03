db5ed2b7bf62defed84996605a985226ddec9d9f
Shreya-TR <shreya465tr@gmail.com>
2026-02-03 15:43:44 +0530

    classifier.py

--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from gorq import Gorq
+from groq import Gorq
 from prompts import SYSTEM_PROMPT
 client = Groq(
     api_key=os.getenv("GROQ_API_KEY"))
@@ -21,3 +21,4 @@ def classify_po(po_description:str,Supplier:str="Not provided"):
 
   )
   return response.choices[0].message.content
+
